48352e77e9546f706a5d758009f12746bea83426
mugabodannyshafi <mugabodannyshafi@gmail.com>
2024-03-12 00:11:00 +0200

    edited login form

--- Js/sign-in.js
+++ Js/sign-in.js
@@ -1,38 +1,37 @@
    
     document.addEventListener('DOMContentLoaded', () =>{
-    const user = document.getElementById('user')
-    const pass = document.getElementById('pass')
-    const link = document.getElementById('admin-link')
-    const button = document.getElementById('button')
+        const user = document.getElementById('user')
+        const pass = document.getElementById('pass')
+        const link = document.getElementById('admin-link')
+        const button = document.getElementById('button')
 
+        user.addEventListener('input', () => {
+       if(user.value.trim() === '' || pass.value.trim() === '')
+       {
+        button.style.background = '#438db5'
+        button.style.cursor = 'not-allowed'
+        button.disabled = true
     
-    user.addEventListener('input', () => {
-   if(user.value.trim() === '' || pass.value.trim() === '')
-   {
-    button.style.background = '#BBBBBB'
-    button.style.cursor = 'not-allowed'
-    button.disabled = true
-
-   }
-   else{
-    button.style.cursor = 'pointer'
-    button.style.background = '#21A5EE'
-    button.disabled = true
-    link.href = './Admin/admin-index.html'
-   }
-    })
-    pass.addEventListener('input', () => {
-        if(pass.value.trim() === '' || user.value.trim() === '')
-        {
-         button.style.background = '#BBBBBB'
-         button.style.cursor = 'not-allowed'
-         button.disabled = true
-        }
-        else{
-         button.style.cursor = 'pointer'
-         button.style.background = '#21A5EE'
-         button.disabled = false
-         link.href = './Admin/admin-index.html'    
-        }
-         })
+       }
+       else{
+        button.style.cursor = 'pointer'
+        button.style.background = '#21A5EE'
+        button.disabled = true
+        link.href = './Admin/admin-index.html'
+       }
         })
+        pass.addEventListener('input', () => {
+            if(pass.value.trim() === '' || user.value.trim() === '')
+            {
+             button.style.background = '#438db5'
+             button.style.cursor = 'not-allowed'
+             button.disabled = true
+            }
+            else{
+             button.style.cursor = 'pointer'
+             button.style.background = '#21A5EE'
+             button.disabled = false
+             link.href = './Admin/admin-index.html'    
+            }
+             })
+            })
